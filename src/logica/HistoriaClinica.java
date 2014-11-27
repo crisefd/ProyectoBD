42aa5f0f3835e51e9e5013df21dfc8e79d3ffd6e
crisefd <crisefd@crisefd-HP-Pavilion-dv4-Notebook-PC>
2014-11-27 11:36:39 -0500

    update Historia Clinica

--- src/logica/HistoriaClinica.java
+++ src/logica/HistoriaClinica.java
@@ -14,5 +14,47 @@ import java.util.Calendar;
 public class HistoriaClinica {
     private String id, id_paciente;
     private Calendar fechaApertura;
+
+    /**
+     * @return the id
+     */
+    public String getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the id_paciente
+     */
+    public String getId_paciente() {
+        return id_paciente;
+    }
+
+    /**
+     * @param id_paciente the id_paciente to set
+     */
+    public void setId_paciente(String id_paciente) {
+        this.id_paciente = id_paciente;
+    }
+
+    /**
+     * @return the fechaApertura
+     */
+    public Calendar getFechaApertura() {
+        return fechaApertura;
+    }
+
+    /**
+     * @param fechaApertura the fechaApertura to set
+     */
+    public void setFechaApertura(Calendar fechaApertura) {
+        this.fechaApertura = fechaApertura;
+    }
     
 }
